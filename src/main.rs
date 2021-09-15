473addb6e13ab0ab64975941238e77b9451bd709
petros21 <petrospapa21@gmail.com>
2021-09-15 19:14:24 +0300

    compatibility adjustments and exe for linux

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use colored::*;
 use mezura::{config_manager, io_handler, *, self};
 
 fn main() {
+    // Only in windows, it is required to enable a virtual terminal environment, so that the colors will display correctly
     #[cfg(target_os = "windows")]
     control::set_virtual_terminal(true).unwrap();
 
