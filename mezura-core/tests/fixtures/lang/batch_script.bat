9eff91fa6e0af2f3a38eefa230e5ae22785a7925
Petros Papa <petrospapa21@gmail.com>
2026-08-25 17:31:52 +0300

    Batch language support

---
+++ mezura-core/tests/fixtures/lang/batch_script.bat
@@ -0,0 +1,9 @@
+rem mezura-expect lines=9 code=3 comments=4 extra=2
+@echo off
+
+REM the uppercase spelling opens a comment too
+:: and so does the double colon
+Rem and the capitalised one
+
+set GREETING="hello world"
+echo %GREETING%
